8f066c19163d0ae906ba2acd25ec3b8eb9204df4
Josh <jcheng115@gmail.com>
2018-06-03 13:26:20 -0700

    added about page with some styles

---
+++ scripts/reveal.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.about-me', { duration: 1000 });
+sr.reveal('.about-content', { duration: 1000 });
+sr.reveal('.about-me', { duration: 1000 });
+sr.reveal('.single', { duration: 1000 });

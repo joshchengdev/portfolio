403b9387db63b3f8816ea61a6a7ea85acbd6e46e
Josh <jcheng115@gmail.com>
2018-06-04 01:33:54 -0700

    Restyled headline with triangle and illustration

--- scripts/reveal.js
+++ scripts/reveal.js
@@ -1,5 +1,5 @@
 window.sr = ScrollReveal({ reset: true });
-
+sr.reveal('.headline', { duration: 1100 });
 sr.reveal('.about-me', { duration: 1000 });
 sr.reveal('.about-content', { duration: 1000 });
 sr.reveal('.about-me', { duration: 1000 });

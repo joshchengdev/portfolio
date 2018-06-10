821fb2ac66f1d960522c166734d8d74dac67fca3
Josh <jcheng115@gmail.com>
2018-06-09 17:00:26 -0700

    added mockup imgs for work

--- scripts/reveal.js
+++ scripts/reveal.js
@@ -3,4 +3,4 @@ sr.reveal('.headline', { duration: 1100 });
 sr.reveal('.about-me', { duration: 1000 });
 sr.reveal('.about-content', { duration: 1000 });
 sr.reveal('.about-me', { duration: 1000 });
-sr.reveal('.single', { duration: 1000 });
+sr.reveal('.work-container', { duration: 1000 });

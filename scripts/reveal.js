6a9459730d92713be5ae31d8249617f1a81265af
Josh <jcheng115@gmail.com>
2018-06-10 19:16:34 -0700

    Individual work page mockups and fixed header

--- scripts/reveal.js
+++ scripts/reveal.js
@@ -1,6 +1,5 @@
 window.sr = ScrollReveal({ reset: true });
-sr.reveal('.headline', { duration: 1100 });
-sr.reveal('.about-me', { duration: 1000 });
-sr.reveal('.about-content', { duration: 1000 });
-sr.reveal('.about-me', { duration: 1000 });
-sr.reveal('.work-container', { duration: 1000 });
+sr.reveal('.headline', { duration: 900 });
+sr.reveal('.about-me', { duration: 700 });
+
+sr.reveal('.work-container', { duration: 700 });

a2a7df267985abd4d1c74187ec2d4aea056de3f6
Josh <jcheng115@gmail.com>
2018-06-25 12:02:20 -0700

    google analytics

--- scripts/reveal.js
+++ scripts/reveal.js
@@ -3,3 +3,4 @@ sr.reveal('.headline', { duration: 900 });
 sr.reveal('.about-me', { duration: 700 });
 
 sr.reveal('.work-container', { duration: 700 });
+sr.reveal('.individual-work-page', {duration: 700});

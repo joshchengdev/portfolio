a051a8d46038394bf1127ef57985b4195fccf15d
Josh <jcheng115@gmail.com>
2019-01-04 01:52:00 -0800

    Added two new works and updated titles

--- scripts/reveal.js
+++ scripts/reveal.js
@@ -1,6 +1,5 @@
 window.sr = ScrollReveal({ reset: true });
-sr.reveal('.headline', { duration: 900 });
-sr.reveal('.about-me', { duration: 700 });
+sr.reveal(".headline", { duration: 900 });
+sr.reveal(".about-me", { duration: 700 });
 
-sr.reveal('.work-container', { duration: 700 });
-sr.reveal('.individual-work-page', {duration: 700});
+sr.reveal(".work-container", { duration: 700 });
